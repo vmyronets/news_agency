6855eedc1e2bb1e3712b774db1eb36a3f7d7e8d5
Volodymyr Myronets <volodymyrmyronets@gmail.com>
2025-05-10 22:07:21 +0300

    Add model Newspaper to models.py

--- news/models.py
+++ news/models.py
@@ -7,7 +7,8 @@ class Topic(models.Model):
     name = models.CharField(max_length=255)
 
     class Meta:
-        verbose_name_plural = "Topics"
+        verbose_name = "topic"
+        verbose_name_plural = "topics"
         ordering = ["name"]
 
     def __str__(self):
@@ -15,10 +16,29 @@ class Topic(models.Model):
 
 
 class Redactor(AbstractUser):
-    years_of_experience = models.PositiveIntegerField(default=0)
+    years_of_experience = models.PositiveIntegerField(default=0, blank=True)
 
     class Meta:
+        verbose_name = "redactor"
+        verbose_name_plural = "redactors"
         ordering = ["username"]
 
     def __str__(self):
-        return self.username
+        return f"{self.username} ({self.first_name} {self.last_name})"
+
+
+class Newspaper(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    published_date = models.DateTimeField(auto_now_add=True)
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE,
+                              related_name="newspapers")
+    publishers = models.ManyToManyField(Redactor, related_name="newspapers")
+
+    class Meta:
+        verbose_name = "newspaper"
+        verbose_name_plural = "newspapers"
+        ordering = ["-published_date"]
+
+    def __str__(self):
+        return self.title
