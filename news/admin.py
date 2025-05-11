2986389d87a73f0e19ee3367071681a70b224282
Volodymyr Myronets <volodymyrmyronets@gmail.com>
2025-05-11 19:03:31 +0300

    Configure Redactor model with custom admin interface in admin.py

--- news/admin.py
+++ news/admin.py
@@ -3,3 +3,23 @@ from django.contrib.auth.admin import UserAdmin
 from .models import Newspaper, Redactor, Topic
 
 
+@admin.register(Redactor)
+class RedactorAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("years_of_experience",)
+    fieldsets = UserAdmin.fieldsets + (
+        (("Additional info", {"fields": ("years_of_experience",)}),)
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            (
+                "Additional info",
+                {
+                    "fields": (
+                        "first_name",
+                        "last_name",
+                        "years_of_experience",
+                    )
+                },
+            ),
+        )
+    )
