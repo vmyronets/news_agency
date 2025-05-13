c82e7abafbb4a97fca73730fe4cfa5db1786c9ec
Volodymyr Myronets <volodymyrmyronets@gmail.com>
2025-05-13 12:53:10 +0300

    Update URL patterns to include Topic create view

--- news/urls.py
+++ news/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
-from .views import index
+from news.views import(
+    index,
+    TopicListView,
+    TopicDetailView,
+
+)
 
 
 urlpatterns = [
     path("", index, name="index"),
-    path("topics/", TopicListView.as_view(), name="topics"),
+    path("topics/", TopicListView.as_view(), name="topic-list"),
+    path("topics/create/", TopicCreatelView.as_view(), name="topic-create"),
 ]
 
 app_name = "news"
