bb3111a7682eea8acd1f700093e70582443e12c1
Volodymyr Myronets <volodymyrmyronets@gmail.com>
2025-05-14 20:01:45 +0300

    Refactor URL patterns for better readability and consistency

--- news/urls.py
+++ news/urls.py
@@ -17,11 +17,19 @@ from news.views import (
     RedactorDeleteView,
 )
 
-
 urlpatterns = [
     path("", index, name="index"),
-    path("topics/", TopicListView.as_view(), name="topic-list"),
-    path("topics/create/", TopicCreateView.as_view(), name="topic-create"),
+
+    path(
+        "topics/",
+        TopicListView.as_view(),
+        name="topic-list"
+    ),
+    path(
+        "topics/create/",
+        TopicCreateView.as_view(),
+        name="topic-create"
+    ),
     path(
         "topics/<int:pk>/update/",
         TopicUpdateView.as_view(),
@@ -32,11 +40,10 @@ urlpatterns = [
         TopicDeleteView.as_view(),
         name="topic-delete"
     ),
-    path("newspapers/", NewspaperListView.as_view(), name="newspaper-list"),
     path(
-        "newspapers/<int:pk>/",
-        NewspaperDetail.as_view(),
-        name="newspaper-detail"
+        "newspapers/",
+        NewspaperListView.as_view(),
+        name="newspaper-list"
     ),
     path(
         "newspapers/<int:pk>/",
@@ -47,18 +54,22 @@ urlpatterns = [
         "newspapers/create/",
         NewspaperCreateView.as_view(),
         name="newspaper-create"
-         ),
+    ),
     path(
         "newspapers/<int:pk>/update/",
         NewspaperUpdateView.as_view(),
         name="newspaper-update"
-         ),
+    ),
     path(
         "newspapers/<int:pk>/delete/",
-         NewspaperDeleteView.as_view(),
-         name="newspaper-delete"
-         ),
-    path("redactors/", RedactorListView.as_view(), name="redactor-list"),
+        NewspaperDeleteView.as_view(),
+        name="newspaper-delete"
+    ),
+    path(
+        "redactors/",
+        RedactorListView.as_view(),
+        name="redactor-list"
+    ),
     path(
         "redactors/<int:pk>",
         RedactorDetailView.as_view(),
@@ -68,14 +79,16 @@ urlpatterns = [
         "redacrors/create/",
         RedactorCreateView.as_view(),
         name="redactor-create"
-         ),
+    ),
     path("redactors/<int:pk>/update/",
          RedactorUpdateView.as_view(),
          name="redactor-update"
          ),
-    path("redactors/<int:pk>/delete/",
-         RedactorDeleteView.as_view(),
-         name="redactor-delete")
+    path(
+        "redactors/<int:pk>/delete/",
+        RedactorDeleteView.as_view(),
+        name="redactor-delete"
+    )
 ]
 
 app_name = "news"
