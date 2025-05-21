976dfb9862425bd7da398452b5d01cb540d57d82
Volodymyr Myronets <volodymyrmyronets@gmail.com>
2025-05-21 21:04:10 +0300

    Update context processor path in settings.py

--- news_agency/settings.py
+++ news_agency/settings.py
@@ -69,7 +69,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'apps.context_processors.cfg_assets_root'
+                'news.context_processors.cfg_assets_root'
             ],
         },
     },
