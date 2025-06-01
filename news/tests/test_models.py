35a3524d92171ddb3d85df27b578596b64089c31
Volodymyr Myronets <volodymyrmyronets@gmail.com>
2025-06-01 13:36:21 +0300

    Add tests for Newspaper model and type hints to existing test methods

--- news/tests/test_models.py
+++ news/tests/test_models.py
@@ -1,11 +1,11 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 
-from news.models import Topic
+from news.models import Topic, Newspaper
 
 
 class TestTopicModel(TestCase):
-    def test_topic_str(self):
+    def test_topic_str(self) -> None:
         """Test the string representation of a Topic model."""
         topic = Topic(name="test")
         self.assertEqual(str(topic), topic.name)
@@ -22,7 +22,7 @@ class TestRedactorModel(TestCase):
             years_of_experience=11,
         )
 
-    def test_redactor_str(self):
+    def test_redactor_str(self) -> None:
         """Test the string representation of a Redactor model."""
         self.assertEqual(
             str(self.redactor),
@@ -39,9 +39,21 @@ class TestRedactorModel(TestCase):
         self.assertEqual(self.redactor.years_of_experience, year_of_experience)
         self.assertTrue(self.redactor.check_password(password))
 
-    def test_get_absolute_url(self):
+    def test_get_absolute_url(self) -> None:
         """Test the redactor's get_absolute_url method."""
         self.assertEqual(
             self.redactor.get_absolute_url(),
             "/redactors/1/"
         )
+
+
+class TestNewspaperModel(TestCase):
+    def test_newspaper_str(self) -> None:
+        """Test the string representation of a Newspaper model."""
+        topic = Topic.objects.create(name="test_topic")
+        newspaper = Newspaper.objects.create(
+            title="test_title",
+            content="test_content",
+            topic=topic,
+        )
+        self.assertEqual(str(newspaper), newspaper.title)
