18730d089dc228f312c584982bb0a7146314110d
Volodymyr Myronets <volodymyrmyronets@gmail.com>
2025-06-01 12:52:58 +0300

    Add test for Topic model string representation

--- news/tests/test_models.py
+++ news/tests/test_models.py
@@ -1,1 +1,11 @@
 from django.test import TestCase
+
+from news.models import Topic
+
+
+class TestTopicModel(TestCase):
+    def test_topic_str(self):
+        """Test the string representation of a Topic model."""
+        topic = Topic(name="test")
+        self.assertEqual(str(topic), topic.name)
+
