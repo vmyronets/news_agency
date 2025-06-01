3474269a475c0a0d9fb20007b88561e637d904fa
Volodymyr Myronets <volodymyrmyronets@gmail.com>
2025-06-01 12:55:54 +0300

    Add test for Redactor model string representation

--- news/tests/test_models.py
+++ news/tests/test_models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 
 from news.models import Topic
@@ -8,4 +9,25 @@ class TestTopicModel(TestCase):
         """Test the string representation of a Topic model."""
         topic = Topic(name="test")
         self.assertEqual(str(topic), topic.name)
+
+
+class TestRedactorModel(TestCase):
+    def setUp(self):
+        """Set up common test data."""
+        self.redactor = get_user_model().objects.create_user(
+            username="test_name",
+            password="test_password",
+            first_name="test_first_name",
+            last_name="test_last_name",
+            years_of_experience=11,
+        )
+
+    def test_redactor_str(self):
+        """Test the string representation of a Redactor model."""
+        self.assertEqual(
+            str(self.redactor),
+            f"{self.redactor.username} "
+            f"({self.redactor.first_name} {self.redactor.last_name})"
+        )
+
 
