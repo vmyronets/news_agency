3c5d400c32b093c25f03cd95bdb60ca15b70d5ff
Volodymyr Myronets <volodymyrmyronets@gmail.com>
2025-06-08 22:39:12 +0300

    Make changes for development and production environments

---
+++ news_agency/settings/prod.py
@@ -0,0 +1,29 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = ["127.0.0.1", ]
+
+# Database
+# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': "django.db.backends.postgresql",
+        'NAME': os.getenv("POSTGRES_DB"),
+        'USER': os.getenv("POSTGRES_USER"),
+        'PASSWORD': os.getenv("POSTGRES_PASSWORD"),
+        'HOST': os.getenv("POSTGRES_HOST"),
+        'PORT': int(os.getenv("POSTGRES_DB_PORT")),
+        'OPTIONS': {
+            'sslmode': 'require'
+        }
+    }
+}
+
+SESSION_COOKIE_SECURE = True
+
+SECURE_SSL_REDIRECT = True
+
+CSRF_COOKIE_SECURE = True
