d533b232cef7243d94bb51f77bb2c908da05da7f
Volodymyr Myronets <volodymyrmyronets@gmail.com>
2025-06-08 13:59:36 +0300

    Add istalled programs to requirements.txt

--- news_agency/urls.py
+++ news_agency/urls.py
@@ -1,7 +1,7 @@
 """
-URL configuration for news_agency project.
+URL configuration for a news_agency project.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
+The `urlpatterns` list routes URLs to views. For more information, please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
 Examples:
 Function views
@@ -14,7 +14,6 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from debug_toolbar.toolbar import debug_toolbar_urls
 
 from django.contrib import admin
 from django.urls import path, include
@@ -23,4 +22,4 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("news.urls", namespace="news")),
     path("accounts/", include("django.contrib.auth.urls"))
-] + debug_toolbar_urls()
+]
