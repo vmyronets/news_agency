18caa8924469bbdccf27b9a941093acd6b14124b
Volodymyr Myronets <volodymyrmyronets@gmail.com>
2025-06-08 23:53:00 +0300

    Add RENDER_EXTERNAL_HOSTNAME variable to prod.py

--- news_agency/settings/prod.py
+++ news_agency/settings/prod.py
@@ -3,7 +3,11 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1", ]
+ALLOWED_HOSTS = ["127.0.0.1"]
+
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
